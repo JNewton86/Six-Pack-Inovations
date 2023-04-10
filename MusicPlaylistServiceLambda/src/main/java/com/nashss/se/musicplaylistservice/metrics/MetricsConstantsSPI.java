01ef5ebfd4e0fbcd54328e674ff4638e2fb6e744
Lina Duangsay <linaduangsay@gmail.com>
2023-04-10 15:11:14 -0500

    added a base for the metric constants, will need to be revisited when ready to deploy to cloudwatch

---
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/metrics/MetricsConstantsSPI.java
@@ -0,0 +1,15 @@
+package com.nashss.se.musicplaylistservice.metrics;
+
+public class MetricsConstantsSPI {
+
+
+    public static final String GETORDER_ORDERNOTFOUND_COUNT = "GetOrder.OrderNotFoundException.Count";
+    public static final String GETBEER_BEERNOTFOUND_COUNT = "GetBeer.BeerNotFoundException.Count";
+
+    public static final String UPDATEINVENTORY_INVALIDATTRIBUTEVALUE_COUNT =
+            "UpdateInventory.InvalidAttributeValueException.Count";
+
+    public static final String SERVICE = "Service";
+    public static final String SERVICE_NAME = "SixPackInnovationsService";
+    public static final String NAMESPACE_NAME = "U5/SixPackInnovationsService";
+}
