d4d6bc012b42e7034a7beea1bd1caa4d83b26204
Hillary <hillarymartin.nss@gmail.com>
2023-04-10 13:23:35 -0700

    edits made to Beer.java and beerdata.json created

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/models/Beer.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/models/Beer.java
@@ -9,17 +9,19 @@ public class Beer {
     private String name;
     private Enum PackagingType;
     private BigDecimal unitPrice;
-    private Integer availabeUnits;
+    private Integer availableUnits;
     private Integer reservedUnits;
+    private Integer totalUnits;
 
-    public Beer(String id, Enum beerType, String name, Enum packagingType, BigDecimal unitPrice, Integer availabeUnits, Integer reservedUnits) {
+    public Beer(String id, Enum beerType, String name, Enum packagingType, BigDecimal unitPrice, Integer availableUnits, Integer reservedUnits) {
         this.id = id;
         this.beerType = beerType;
         this.name = name;
         PackagingType = packagingType;
         this.unitPrice = unitPrice;
-        this.availabeUnits = availabeUnits;
+        this.availableUnits = availableUnits;
         this.reservedUnits = reservedUnits;
+        this.totalUnits = availableUnits + reservedUnits;
     }
 
     public String getId() {
@@ -42,12 +44,16 @@ public class Beer {
         return unitPrice;
     }
 
-    public Integer getAvailabeUnits() {
-        return availabeUnits;
+    public Integer getAvailableUnits() {
+        return availableUnits;
     }
 
     public Integer getReservedUnits() {
         return reservedUnits;
+    }
+
+    public Integer getTotalUnits() {
+        return totalUnits;
     }
 
     public void setId(String id) {
@@ -70,24 +76,25 @@ public class Beer {
         this.unitPrice = unitPrice;
     }
 
-    public void setAvailabeUnits(Integer availabeUnits) {
-        this.availabeUnits = availabeUnits;
+    public void setAvailableUnits(Integer availableUnits) {
+        this.availableUnits = availableUnits;
     }
 
     public void setReservedUnits(Integer reservedUnits) {
         this.reservedUnits = reservedUnits;
     }
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Beer beer = (Beer) o;
-        return Objects.equals(id, beer.id) && Objects.equals(beerType, beer.beerType) && Objects.equals(name, beer.name) && Objects.equals(PackagingType, beer.PackagingType) && Objects.equals(unitPrice, beer.unitPrice) && Objects.equals(availabeUnits, beer.availabeUnits) && Objects.equals(reservedUnits, beer.reservedUnits);
+        return Objects.equals(id, beer.id) && Objects.equals(beerType, beer.beerType) && Objects.equals(name, beer.name) && Objects.equals(PackagingType, beer.PackagingType) && Objects.equals(unitPrice, beer.unitPrice) && Objects.equals(availableUnits, beer.availableUnits) && Objects.equals(reservedUnits, beer.reservedUnits);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, beerType, name, PackagingType, unitPrice, availabeUnits, reservedUnits);
+        return Objects.hash(id, beerType, name, PackagingType, unitPrice, availableUnits, reservedUnits);
     }
 }
