dea3eb67659d082694d22db4fa6e15cd00fe6315
Jeff Newton <jeff.newton86@live.com>
2023-04-13 13:20:31 -0500

    wip, lambda not repsonding to curl command

--- MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/lambda/UpdateInventoryLambda.java
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/lambda/UpdateInventoryLambda.java
@@ -6,13 +6,18 @@ import com.nashss.se.musicplaylistservice.activity.results.UpdateInventoryResult
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.util.logging.Logger;
+
 public class UpdateInventoryLambda
     extends LambdaActivityRunner<UpdateInventoryRequest, UpdateInventoryResult>
     implements RequestHandler<AuthenticatedLambdaRequest<UpdateInventoryRequest>, LambdaResponse> {
+
+    //Todo add logger
     @Override
     public LambdaResponse handleRequest(AuthenticatedLambdaRequest<UpdateInventoryRequest> input, Context context) {
         return super.runActivity(
             () -> {
+
                 UpdateInventoryRequest authenticatedRequest = input.fromBody(UpdateInventoryRequest.class);
                 return input.fromUserClaims(claims ->
                         UpdateInventoryRequest.builder()
@@ -20,6 +25,7 @@ public class UpdateInventoryLambda
                                 .withPackagingType(authenticatedRequest.getPackagingType())
                                 .withAvailableUnits(authenticatedRequest.getavailableUnits())
                                 .withReservedUnits(authenticatedRequest.getavreservedUnits())
+                                .withUnitPrice(authenticatedRequest.getUnitPrice())
                                 .build());
             },
             (request, serviceComponent) ->
